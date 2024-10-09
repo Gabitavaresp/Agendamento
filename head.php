86b94dbdd33a05dee837c34926fc94370465fa72
Gabitavaresp <125685828+Gabitavaresp@users.noreply.github.com>
2024-10-09 18:03:02 -0300

    Delete head.php

--- head.php
+++
@@ -1,6 +0,0 @@
-<head>
-	<meta charset="utf-8">
-	<title>Agenda</title>
-	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
-	<script src="../js/bootstrap.js" type="text/javascript"></script>
-</head>
